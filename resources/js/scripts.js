ffd17bd4e2a8e8594a7ad0cc6e6dd41fb79f0426
Sean Newman <seangnewman@msn.com>
2018-03-03 20:25:24 -0700

    Push early, push often, pushed the search button functionality

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,45 @@
+// Wait until all objects in the DOM have loaded 
+$(document).ready(function() {
+    initializeButtons();
+
+    //Listen if the user would like to add a search term 
+    $('#addToTerms').on("click",function(event){
+      //Picked this up in class on 3/3/2018
+      //Prevents the default action on the form
+      event.preventDefault();
+   
+      //Capture the contents of the input box.
+      //if the contents are empty, take no action.
+      var elementText = $('#searchInput').val();
+      if(elementText.length > 0){
+        terms.push(elementText);
+        initializeButtons();
+      }
+         
+    });// End Search Click Event
+
+    
+
+});
+
+ function initializeButtons(){
+    $('#btnContainer').empty();
+     for( var i=0; i < terms.length; i++){
+       createNewButton(terms[i]);
+     }
+ }
+
+ function createNewButton(topic){
+   var theButton = $('<button>');
+   theButton.text(topic);
+   theButton.addClass('btnClass');
+   theButton.attr("data-name", topic);
+   $('#btnContainer').prepend(theButton);
+ }
 
  
+
+
 
 var inc=1;
 	$(function () {
